6e1902d20c971a7c8fb5fb13a9821557d7808bfa
erik emmanuel navarrete reyes <apuntesnavarrete@gmail.com>
2026-06-15 19:22:21 -0600

    roll modficaciones 15-junio

--- src/app/components/rollvisual/rollvisual.component.ts
+++ src/app/components/rollvisual/rollvisual.component.ts
@@ -2,10 +2,11 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import html2canvas from 'html2canvas';
 import { BtnDescargarComponent } from '../utils/btn-descargar/btn-descargar.component';
 import { FilarollComponent } from './filaroll/filaroll.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rollvisual',
-  imports: [BtnDescargarComponent, FilarollComponent], // Si necesitas CommonModule o pipes en el futuro, van aquí
+  imports: [BtnDescargarComponent, FilarollComponent, FormsModule], // Si necesitas CommonModule o pipes en el futuro, van aquí
   templateUrl: './rollvisual.component.html',
   styleUrl: './rollvisual.component.css'
 })
@@ -16,49 +17,20 @@ export class RollvisualComponent {
 // Nueva variable para el día (en mayúsculas para el diseño)
   diaSemana: string = 'LUNES';
 
-// Si esta lista tiene 2 elementos, se dibujarán 2; si tiene 3, se dibujarán 3 de forma automática.
-listaPartidos = [
-  {
-    "categoria": "sub 16",
-    "local": "Juventus",
-    "localLogo": "juventus.png",
-    "visitante": "Arsenal",
-    "visitanteLogo": "arsenal.png",
-    "horario": "7:20pm"
-  },
-  {
-    "categoria": "sub 16",
-    "local": "AC milan",
-    "localLogo": "milan.png",
-    "visitante": "España",
-    "visitanteLogo": "españa.png",
-    "horario": "8:10"
-  },
-  {
-    "categoria": "mixto",
-    "local": "Alemania",
-    "localLogo": "alemania.png",
-    "visitante": "Chelsea",
-    "visitanteLogo": "chelsea.png",
-    "horario": "9:00"
-  },
-  {
-    "categoria": "mixto",
-    "local": "CFT",
-    "localLogo": "cft.png",
-    "visitante": "Bayer",
-    "visitanteLogo": "bayer.png",
-    "horario": "9:50"
-  },
-  {
-    "categoria": "mixto",
-    "local": "Tepatitlan",
-    "localLogo": "tepatitlan.png",
-    "visitante": "Palmeiras",
-    "visitanteLogo": "palmeiras.png",
-    "horario": "10:40"
-  }
-];
+// Esta será la lista que dibuja el póster (empieza con tus datos de ejemplo)
+  listaPartidos = [
+    {
+      "categoria": "sub 16",
+      "local": "Juventus",
+      "localLogo": "juventus.png",
+      "visitante": "Arsenal",
+      "visitanteLogo": "arsenal.png",
+      "horario": "7:20pm"
+    }
+  ];
+
+  // 👈 Nueva variable para el texto que pegues en el HTML
+  jsonTexto: string = JSON.stringify(this.listaPartidos, null, 2);
 
   // Referencia para capturar el contenedor del póster
   @ViewChild('poster', { static: false }) posterElement!: ElementRef;
@@ -68,5 +40,14 @@ listaPartidos = [
     this.numeroCampo = nuevoCampo;
   }
 
+  // 👈 Nueva función para procesar el JSON desde la pantalla
+  actualizarPartidos() {
+    try {
+      this.listaPartidos = JSON.parse(this.jsonTexto);
+      alert('¡Partidos actualizados con éxito en el póster!');
+    } catch (error) {
+      alert('Error: El formato del JSON no es correcto. Revisa las comillas y comas.');
+    }
+  }
 
 }
